7179b2fb74ecc2d7b92cfa185613517910cd0ce0
Jennifer Beem and Koji Nakagawa <jennifer@>
2017-05-03 14:45:17 -0700

    add recent views, and change styles

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'cook',
+      'username' => 'cook',
+      'password' => 'cook',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'GKSslR8fHvuGr1YMkZWq-9bmFGwxhIm71eKvrIjepyw';
 
 /**
  * Base URL (optional).
